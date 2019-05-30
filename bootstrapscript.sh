96f218434e593c9eb750009fb4f897cd4b46875b
Shubhro27 <33026297+Shubhro27@users.noreply.github.com>
2019-05-30 21:50:28 +0530

    Delete bootstrapscript.sh

--- bootstrapscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install httpd -y
-service httpd start
-chkconfig httpd on
-cd /var/www/html
-echo "<html><h1>Hello ALL Welcome To Shubhro's Webpage</h1></html>" > index.html
-aws s3 mb s3://INDEXHTMLBKUP
-aws s3 cp index.html s3://INDEXHTMLBKUP
